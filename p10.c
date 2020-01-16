a5a60da80636a6c67d440f857ebc4e958143297d
Abdullah Abou Mahmoud <abdullah9752@yahoo.com>
2020-01-16 17:12:20 -0500

    Fibonnaci Analytical

---
+++ p10.c
@@ -0,0 +1,48 @@
+/****************************************************************************/
+/*                                                                          */
+/* Programmer: Abdullah Mahmoud                                             */
+/*                                                                          */
+/* Program 10: Fibonacci Analytical                                         */
+/*                                                                          */
+/* Time to Complete: 10 minutes                                             */
+/*                                                                          */
+/****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int nthNumber( double a, double b, double c, int n ) ;
+
+int main( int argc, char *argv[] ) {
+
+  int n ;
+  
+  double a, b, c ;
+  
+  n = atoi( argv[1] ) ;
+  
+  a = 1 / sqrt( 5 ) ;
+  b = pow( (( 1 + sqrt( 5 ) ) / 2 ) , n ) ;
+  c = pow( (( 1 - sqrt( 5 ) ) / 2 ) , n ) ; 
+
+  printf( "\nThe %dth number is %d\n", n, nthnumber( a, b, c, n ) ) ;
+
+  return 0 ;
+  
+}
+
+int nthNumber( double a, double b, double c, int n ) {
+
+  int f ;
+  
+  if      ( n == 1 ) { f = 1 ; }  
+  else if ( n == 2 ) { f = 1 ; }
+  else if ( n == 3 ) { f = 2 ; }
+  else if ( n == 4 ) { f = 3 ; }
+
+  else f =  a * ( b - c ) ;
+ 
+  return f ;
+    
+}
